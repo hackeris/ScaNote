9759007eafc1459ce2763e889854a812217e39aa
hackeris <hackeris@qq.com>
2016-01-14 00:24:55 +0800

    hackeris - add unit test demo

---
+++ src/androidTest/scala/com/rainm/scala/android/ApplicationTest.scala
@@ -0,0 +1,18 @@
+package com.rainm.scala.android
+
+import android.test.AndroidTestCase
+
+/**
+  * Created by hackeris on 16/1/14.
+  */
+
+class ApplicationTest extends AndroidTestCase {
+
+  def ApplicationTest(): Unit = {
+  }
+
+  def testDemo(): Unit = {
+    assert(true)
+  }
+
+}
